94f1b8c01992055505bc6904b882fb99facbdcf7
Borja Lorente <blorente.me@gmail.com>
2026-02-03 10:19:51 +0000

    feat: Add support for syncd
    checkpoint: I can build syncd binary
    
    checkpoint: Build saidump, saiplayer, and saisdkdump
    
    feat: Build saiasiccmp and saidiscovery
    
    checkpoint: Build syncd_pkg
    
    checkpoint: Create dev packages for saivs
    
    fix: Introduce asan to the Bazel buid
    
    paramterize one mtree per platform
    
    cleanup: run buildifier
    
    fix: Add platform-dependenc mtrees
    
    cleanup: Move loose starlark to bazel/
    
    fix saiasiccmp tests
    
    fix: Introduce sailib target to encapsulate headers and select sai based on flag
    
    WIP: Add support for saithrift
    
    feat: Remove TODO BL: comments

--- saiasiccmp/test.sh
+++ saiasiccmp/test.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 EXIT_VALUE=0
+saiasiccmp=${1:-./saiasiccmp}
+dump1=${2:-dump1.json}
+dump2=${3:-dump2.json}
+dump3=${4:-dump3.json}
+
+# When run under Bazel, args are rlocation paths; prepend TEST_SRCDIR to resolve them.
+if [ -n "${TEST_SRCDIR:-}" ]; then
+    saiasiccmp="$TEST_SRCDIR/$saiasiccmp"
+    dump1="$TEST_SRCDIR/$dump1"
+    dump2="$TEST_SRCDIR/$dump2"
+    dump3="$TEST_SRCDIR/$dump3"
+fi
 
 function test_positive()
 {
-    ./saiasiccmp dump1.json dump2.json
+    "${saiasiccmp}" "${dump1}" "${dump2}"
 
     if [ $? != 0 ]; then
         echo "${FUNCNAME[0]} ERROR: expected dumps to be equal"
@@ -14,7 +26,7 @@ function test_positive()
 
 function test_negative()
 {
-    ./saiasiccmp dump1.json dump3.json
+    "${saiasiccmp}" "${dump1}" "${dump3}"
 
     if [ $? == 0 ]; then
         echo "${FUNCNAME[0]} ERROR: expected dumps to be not equal"
